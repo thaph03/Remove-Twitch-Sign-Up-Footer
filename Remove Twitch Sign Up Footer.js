214382357dc40c448956f05a4eb9db713c3c2b6e
Thai Pham <thaiphm0303@gmail.com>
2024-06-10 21:49:30 -0700

    first commit on tampermonkey scripts

---
+++ Remove Twitch Sign Up Footer.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Remove Twitch Sign Up Footer
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  delete sign up pop up on twitch
+// @author       Thai Pham
+// @match        *://*.twitch.tv/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function removeSignUpPopUp() {
+        const popUp = document.querySelector(".ceAbGI");
+        if (popUp) {
+            popUp.style.display = "none";
+        } else {
+            // Try again in 500ms if the pop-up is not found
+            setTimeout(removeSignUpPopUp, 500);
+        }
+    }
+
+    // Wait for the DOM to fully load
+    window.addEventListener('load', removeSignUpPopUp);
+})();
